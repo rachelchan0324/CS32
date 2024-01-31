64e06474f5fb734edf2ac80902779d8823288075
Rachel Chan <rachelytchan0324@gmail.com>
2024-01-30 17:37:24 -0800

    READY!

--- proj2/proj2/MapTester.cpp
+++ proj2/proj2/MapTester.cpp
@@ -5,191 +5,75 @@
 using namespace std;
 
 int main () {
-    // MARK: Smallbergs's Test Cases
-    Map msd; // KeyType is std::string, ValueType is double
-    assert(msd.insert("ccc", 80));
-    assert(msd.insert("aaa", 40));
-    assert(msd.insert("ddd", 60));
-    assert(msd.insert("bbb", 20));
-    KeyType k = "xxx";
-    ValueType v;
-    assert(!msd.get(4, k, v)  &&  k == "xxx");  // x is unchanged
-    assert(msd.get(1, k, v)  &&  k == "bbb");   // "bbb" is greater than exactly 1 item
-    
-    /** merge()*/
+    KeyType key = "";
+    ValueType value = -1;
+
+    /** default constructor and empty */
+    Map test;
+    assert(test.empty() && test.size() == 0); // should be empty
+
+    /** insert */
+    assert(test.insert("a", 23) && test.insert("b", 46) && test.insert("c", 93) && test.size() == 3 && !test.empty()); // tests insert(), insert 3 values
+    assert(!test.insert("c", 1)); // should not insert if the key already exists
+
+    /** copy constructor*/
+    Map cpy = Map(test); // tests copy constructor
+    assert(cpy.size() == 3); // cpy should have the same contents as test
+    assert(cpy.contains("a") && cpy.contains("b") && cpy.contains("c")); // tests copy constructor and contains method
+
+    /** update, insert, and insertOrUpdate*/
+    assert(cpy.update("a", 32) && cpy.get("a", value) && value == 32); // update existing key
+    assert(!cpy.update("d", 400) && cpy.size() == 3); // does not update if the key doesn't exist
+    assert(cpy.insertOrUpdate("a", 23) && cpy.size() == 3 && cpy.get(0, key, value) && key == "a" && value == 23); // key already exists, should update
+    assert(cpy.insertOrUpdate("d", 408) && cpy.size() == 4 && cpy.get("d", value) && value == 408); // key doesn't exist, should insert
+    assert(!test.contains("d")); // tests copy constructor, "d" should not be added to test; cpy should not be a reference to test
+
+    /** erase*/
+    assert(!cpy.erase("z")); // erasing a key that doesn't exist does nothing
+    assert(cpy.erase("d") && cpy.size() == 3 && !cpy.contains("d")); // erasing a key that exists
+
+    /** get */
+    assert(!test.get("z", value) && value == 408); // getting a key that doesn't exist, shouldn't change anything
+    assert(test.get("c", value) && value == 93); // getting a value of a key that exists
+    assert(cpy.size() == 3 && cpy.get(0, key, value) && key == "a" && value == 23 && cpy.get(1, key, value) && key == "b" && value == 46 && cpy.get(2, key, value) && key == "c" && value == 93); // tests get(i, key, value)
+    assert(!cpy.get(-1, key, value) && !cpy.get(4, key, value) && key == "c" && value == 93); // if i is greater than size or negative, parameters should not change
+
+    /** assignment operator */
+    Map numbers;
+    assert(numbers.insert("one", 1.1) && numbers.insert("two", 2.2) && numbers.insert("three", 3.3));
+    Map temp(numbers); // for testing swap() later
+    numbers = cpy;
+    assert(cpy.size() == 3 && cpy.get(0, key, value) && key == "a" && value == 23 && cpy.get(1, key, value) && key == "b" && value == 46 && cpy.get(2, key, value) && key == "c" && value == 93); // numbers should now have the contents of cpy, testing assignment operator
+    assert(cpy.insert("d", 123.6) && !numbers.contains("d")); // numbers should not be a reference to cpy
+
+    /** swap() */
+    temp.swap(cpy); // swap copy and temp
+    assert(temp.size() == 4 && temp.get(0, key, value) && key == "a" && value == 23 && temp.get(1, key, value) && key == "b" && value == 46 && temp.get(2, key, value) && key == "c" && value == 93 && temp.get(3, key, value) && key == "d" && value == 123.6); // test should have cpy elements
+    assert(cpy.get("one", value) && value == 1.1 && cpy.get(1, key, value) && key == "three" && value == 3.3 && cpy.size() == 3); // cpy should have temp contents now
+
+    /** merge */
     Map names;
-    assert(names.insert("Fred", 123));
-    assert(names.insert("Ethel", 456));
-    assert(names.insert("Lucy",789));
+    assert(names.insert("Fred", 123) && names.insert("Ethel", 456) && names.insert("Lucy",789));
     Map names1;
-    assert(names1.insert("Lucy", 789));
-    assert(names1.insert("Ricky", 321));
-    Map result;
+    assert(names1.insert("Lucy", 789) && names1.insert("Ricky", 321));
+    Map result(cpy); // result has initial values that should be removed
     assert(merge(names, names1, result));
-    assert(result.size() == 4 && result.get(0, k, v) && k == "Ethel" && v == 456 && result.get(1, k, v) && k == "Fred" && v == 123 && result.get(2, k, v) && k == "Lucy" && v == 789 && result.get(3, k, v) && k == "Ricky" && v == 321);
+    assert(result.size() == 4 && result.get(0, key, value) && key == "Ethel" && value == 456 && result.get(1, key, value) && key == "Fred" && value == 123 && result.get(2, key, value) && key == "Lucy" && value == 789 && result.get(3, key, value) && key == "Ricky" && value == 321); // all matching keys have corresponding values
     names1.insertOrUpdate("Lucy",654);
-    assert(!merge(names, names1, result));
-    assert(result.size() == 3 && result.get(0, k, v) && k == "Ethel" && v == 456 && result.get(1, k, v) && k == "Fred" && v == 123 && result.get(2, k, v) && k == "Ricky" && v == 321);
-    
-    Map m;
-    assert(m.insert("Fred", 123));
-    assert(m.insert("Ethel", 456));
-    assert(m.size() == 2);
-    ValueType v_1 = 42;
-    assert(!m.get("Lucy", v_1)  &&  v_1 == 42);
-    assert(m.get("Fred", v_1)  &&  v_1 == 123);
-    v_1 = 42;
-    KeyType x = "Lucy";
-    assert(m.get(0, x, v_1)  &&  x == "Ethel"  &&  v_1 == 456);
-    KeyType x2 = "Ricky";
-    assert(m.get(1, x2, v_1)  &&  x2 == "Fred"  &&  v_1 == 123);
-    
+    assert(!merge(names, names1, result)); // all matching keys do not have corresponding values; return false
+    assert(result.size() == 3 && result.get(0, key, value) && key == "Ethel" && value == 456 && result.get(1, key, value) && key == "Fred" && value == 123 && result.get(2, key, value) && key == "Ricky" && value == 321); // even if the keys are not corresponding, result map must still be constructed
+
     /** reassign() */
-    Map reassignResult;
+    Map reassignResult = result; // result has initial values that should be removed
     assert(names.insert("Ricky", 321));
     reassign(names, reassignResult);
     KeyType k1, k2, k3;
     ValueType v1, v2, v3;
-    assert(reassignResult.size() == 4 && reassignResult.get(0, k, v) && reassignResult.get(1, k1, v1) && reassignResult.get(2, k2, v2) && reassignResult.get(3, k3, v3) && v != v1 != v2 != v3 && v != 456 && v1 != 123 && v2 != 789 && v3 != 321);
-    
+    assert(reassignResult.size() == 4 && reassignResult.get(0, key, value) && reassignResult.get(1, k1, v1) && reassignResult.get(2, k2, v2) && reassignResult.get(3, k3, v3) && value != v1 != v2 != v3 && value != 456 && v1 != 123 && v2 != 789 && v3 != 321 && reassignResult.get("Fred", value) && value != 123); // all values should be different from each other
     assert(names.erase("Ricky"));
     assert(names.insertOrUpdate("Lucy", 456));
     reassign(names, reassignResult);
-    assert(reassignResult.size() == 3 && reassignResult.get(0, k, v) && reassignResult.get(1, k1, v1) && reassignResult.get(2, k2, v2) && ((v == 123 && v1 == 456 && v2 == 456) || (v == 456 && v1 == 456 && v2 == 123)));
-    
-    // MARK: Rachel's Test Cases
-    
-    KeyType key = "";
-    ValueType value = -1;
-    
-    /** insert() */
-    Map test;
-    assert(test.insert("a", 23));
-    assert(test.insert("b", 46));
-    assert(test.insert("c", 93));
-    assert(!test.insert("c", 1)); // should not insert if the key already exists
-    
-    /**Map(const Map& other)**/
-    Map cpy = Map(test);
-    
-    /** update() */
-    assert(test.update("c", 7));
-    assert(test.get("c", value));
-    assert(value == 7);
-    
-    /** erase() */
-    msd.erase("b"); // erase
-    assert(!msd.get("b", value) && value == 7);
-    
-    /** swap() */
-    Map test1; // swap test and test1
-    assert(test1.insert("d", 109));
-    assert(test1.insert("e", 58));
-    test.swap(test1);
-    assert(test.get(0, key, value) && key == "d" && value == 109);
-    assert(test.get(1, key, value) && key == "e" && value == 58);
-    
-    // MARK: Brandon's Test Cases
-    Map bMap;
-    // Empty Map Test Cases
-    assert(bMap.size() == 0);
-    assert(bMap.empty());
-    
-    // Insert Test Cases
-    assert(bMap.insert("Brandon", 1.1));
-    assert(bMap.insert("Rachel", 2));
-    assert(bMap.insert("Sanjay", 3));
-    assert(bMap.insert("Brayden", 4));
-    assert(!bMap.insert("Brandon", 1.1));
-    assert(bMap.size() == 4);
-    
-    // Update Test Cases
-    assert(bMap.update("Rachel", 2.2));
-    assert(!bMap.update("Jon", 1));
-    assert(bMap.size() == 4);
-    
-    // InsertOrUpdate Test Cases
-    assert(bMap.insertOrUpdate("Brayden", 4.4));
-    assert(bMap.insertOrUpdate("Jon", 5));
-    
-    // Erase Test Cases
-    assert(bMap.erase("Brayden"));
-    assert(!bMap.erase("Andrew"));
-    assert(bMap.size() == 4);
-    
-    // Contains Test Cases
-    assert(bMap.contains("Brandon"));
-    assert(!bMap.contains(""));
-    assert(!bMap.contains("Brayden"));
-    
-    // 1st Get Test Cases
-    KeyType key1 = "Ellen";
-    ValueType value1 = 9.99;
-    assert(!bMap.get(key1, value1));
-    assert(value1 == 9.99);
-    key1 = "Rachel";
-    assert(bMap.get(key1, value1));
-    assert(value1 == 2.2);
-    
-    // 2nd Get Test Cases
-    KeyType key2 = "Katie";
-    ValueType value2 = 9.99;
-    assert(!bMap.get(-1, key2, value2));
-    assert(!bMap.get(bMap.size(), key2, value2));
-    assert(key2 == "Katie" && value2 == 9.99);
-    assert(bMap.get(2, key2, value2));
-    assert(key2 == "Rachel" && value2 == 2.2);
-    
-    // Swap Test Cases
-    Map m1;
-    assert(m1.insert("a", 1));
-    assert(m1.insert("b", 2));
-    Map m2;
-    assert(m2.insert("x", 3));
-    m1.swap(m2);
-    assert(m1.size() == 1 && m2.size() == 2);
-    KeyType key3 = "a";
-    ValueType value3;
-    assert(!m1.get(key3, value3));
-    assert(m2.get(key3, value3) && value3 == 1);
-    
-    // Merge Test Cases
-    Map m3;
-    assert(m3.insert("Fred", 123));
-    assert(m3.insert("Ethel", 456));
-    assert(m3.insert("Lucy", 789));
-    Map m4;
-    assert(m4.insert("Lucy", 789));
-    assert(m4.insert("Ricky", 321));
-    Map bResult;
-    assert(bResult.insert("Your Mother", 69420));
-    assert(merge(m3, m4, bResult));
-    assert(bResult.size() == 4);
-    Map m5;
-    assert(m5.insert("Fred", 321));
-    assert(!merge(m3, m5, bResult));
-    assert(bResult.size() == 2);
-    
-    // Reassign Test Cases
-    Map dance;
-    assert(dance.insert("Fred", 1));
-    assert(dance.insert("Ethel", 2));
-    assert(dance.insert("Lucy", 3));
-    assert(dance.insert("Ricky", 4));
-    Map scrambled;
-    assert(scrambled.insert("Love", 92523));
-    reassign(dance, scrambled);
-    ValueType fredNum;
-    ValueType ethelNum;
-    ValueType lucyNum;
-    ValueType rickyNum;
-    assert(scrambled.get("Fred", fredNum));
-    assert(scrambled.get("Ethel", ethelNum));
-    assert(scrambled.get("Lucy", lucyNum));
-    assert(scrambled.get("Ricky", rickyNum));
-    assert(fredNum != 1 && ethelNum != 2 && lucyNum != 3 && rickyNum != 4);
-    assert(fredNum != ethelNum != lucyNum != rickyNum);
-    
+    assert(reassignResult.size() == 3 && reassignResult.get(0, key, value) && reassignResult.get(1, k1, v1) && reassignResult.get(2, k2, v2) && ((value == 123 && v1 == 456 && v2 == 456) || (value == 456 && v1 == 456 && v2 == 123))); // if two keys have the same value
+
     cerr << "Passed all test cases." << endl;
 }
-
