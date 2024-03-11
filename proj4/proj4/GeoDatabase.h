74df5354af4ac8fb8ada304562a08ee418c7cd02
Rachel Chan <rachelytchan0324@gmail.com>
2024-03-11 11:43:14 -0700

    ready to start GeoDatabase

---
+++ proj4/proj4/GeoDatabase.h
@@ -0,0 +1,14 @@
+#include <base_classes.h"
+
+class GeoDatabase: public GeoDatabaseBase {
+public:
+    GeoDatabase();
+    virtual ~GeoDatabase();
+    virtual bool load(const std::string& map_data_file);
+    virtual bool get_poi_location(const std::string& poi,
+                                  GeoPoint& point) const;
+    virtual std::vector<GeoPoint> get_connected_points(const GeoPoint& pt)
+    const;
+    virtual std::string get_street_name(const GeoPoint& pt1,
+                                        const GeoPoint& pt2) const;
+};
